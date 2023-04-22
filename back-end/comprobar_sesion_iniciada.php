5378a25520f4bf2b2f3f27cc475112049303fe18
Alvaro-pc <alvaro.sobrino.coding@gmail.com>
2023-04-22 13:38:30 +0200

    0.0.0.8 fase 0 22/04/2023

--- back-end/comprobar_sesion_iniciada.php
+++ back-end/comprobar_sesion_iniciada.php
@@ -1,7 +1,14 @@
 <?php
     $conexion = new mysqli("localhost", "root", "", "tienda");
 
-    $email = $_POST["email"]; // Obte
+    $email = $_POST["email"];
+    $consulta = "SELECT * FROM usuarios WHERE email = '$email'";
+    $resultado = $conexion->query($consulta);
 
+    if ($resultado->num_rows > 0) {
+        echo "La cuenta si existe";
+    } else {
+        echo "La cuenta no existe";
+    }
     
 ?>
--- front-end/script/comprobar_sesion_iniciada.js
+++ front-end/script/comprobar_sesion_iniciada.js
@@ -1,16 +1,47 @@
-window.addEventListener("load", functiones)
+window.addEventListener("load", funciones)
 function funciones() {
     //comprobamos si habia una sesion iniciada o no.
     if (localStorage.getItem("usuario")) {
         //comprobar que el email esta bien
-
+        conexion()
         //si el email no es valido borrar el localstorage y llamar a la funcion ocultar_contenido()
     } else {
         ocultar_contenido()
     }  
 
-    function ocultar_contenido(){//si la sesion 
+    function ocultar_contenido(){//si la sesion no esta iniciada o esta mal
+        let archivo_actual = window.location.pathname;
+        if(archivo_actual.includes("inicio.html")){
+            document.getElementById("seccion_usuario").style.display="none";
+            document.getElementById("seccion_carrito").style.display="none";
+        }
+        if(archivo_actual.includes("contacto.html")){
+            document.getElementById("seccion_usuario").style.display="none";
+            document.getElementById("seccion_carrito").style.display="none";
+            document.getElementById("sesion_no_iniciada").style.display = "block";
+            
+        }
+        if(archivo_actual.includes("productos.html")){
+            document.getElementById("seccion_usuario").style.display="none";
+            document.getElementById("seccion_carrito").style.display="none";
+        }
+    }
 
+    function conexion(){
+        let formulario = new FormData();
+        formulario.append("email", localStorage.getItem("usuario"))
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "../back-end/comprobar_sesion_iniciada.php")
+        xhr.addEventListener("load", (respuesta)=>{
+            let resultado = respuesta.target.response;
+            if(resultado.includes("La cuenta si existe")){
+                //no hacer cambios
+            }else{
+                ocultar_contenido()
+            }
+        })
+        xhr.send(formulario);
     }
 
 }
